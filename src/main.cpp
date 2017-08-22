ae6d21d6baea75a53ebbb35ec691983acdf37f36
ramesh <ramesh@172.25.104.232>
2017-08-22 14:37:05 +0800

    Phase 1 complete,

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ typedef unsigned char uchar;
 const int k = 30;
 const double error_rate = 0.04;
 
-ull nucl_bm[256] = { 0 };
+ull nucl_bm[256] = { 0 }, rc_bm[256] = { 0 };
 char bm_nucl[4] = { 'A', 'C', 'G', 'T' };
 
 int nucls_to_ulls(int nucls) {
@@ -37,7 +37,7 @@ struct kmer_t {
 
 	kmer_t() : nucls(0), kmer(NULL) {}
 
-	kmer_t(std::string& s) {
+	kmer_t(string& s, bool rc = false) {
 		nucls = s.size();
 		int ints = nucls_to_ulls(nucls);
 		kmer = new ull[ints];
@@ -49,13 +49,13 @@ struct kmer_t {
 				intid++;
 				kmer[intid] = 0;
 			}
-			kmer[intid] |= (nucl_bm[s[i]] << offset);
+			kmer[intid] |= ((rc ? rc_bm[s[nucls-1-i]] : nucl_bm[s[i]]) << offset);
 			offset -= 2;
 		}
 	}
 
 	char get_nucl(int pos) {
-		return bm_nucl[(kmer[pos/32] >> 2*(32-pos%32)-2) & 3];
+		return bm_nucl[((kmer[pos/32] >> (2*(32-pos%32)-2))) & 3];
 	}
 
 	string to_bin_str() {
@@ -128,7 +128,7 @@ struct kmer_t {
 	}
 };
 
-struct kmer_hash {
+struct kmer_p_hash {
 	size_t operator()(const kmer_t* kmer) const {
 		return hash<ull>() (kmer->kmer[0]);
 	}
@@ -146,12 +146,6 @@ int kmer_p_cmp(kmer_t* const k1, kmer_t* const k2) {
 
 struct kmer_p_eq {
 	bool operator () (kmer_t* const k1, kmer_t* const k2) const {
-//		if (k1->nucls != k2->nucls) return false;
-//		int ulls = nucls_to_ulls(k1->nucls);
-//		for (int i = 0; i < ulls; i++) {
-//			if (k1->kmer[i] != k2->kmer[i]) return false;
-//		}
-//		return true;
 		return kmer_p_cmp(k1, k2) == 0;
 	}
 };
@@ -167,8 +161,8 @@ struct pair_t {
 	pair_t(kmer_t* r1, kmer_t* r2) : r1(r1), r2(r2), first1(NULL), last1(NULL), first2(NULL), last2(NULL) {}
 };
 
-typedef unordered_map<kmer_t*, vector<pair<pair_t*, uchar> >, kmer_hash, kmer_p_eq> kmer_indexed_pairs;
-typedef unordered_map<kmer_t*, vector<pair<pair_t*, pair<uchar, uchar> > >, kmer_hash, kmer_p_eq> kmer_indexed_pairs_2pos;
+typedef unordered_map<kmer_t*, vector<pair<pair_t*, uchar> >, kmer_p_hash, kmer_p_eq> kmer_indexed_pairs;
+typedef unordered_map<kmer_t*, vector<pair<pair_t*, pair<uchar, uchar> > >, kmer_p_hash, kmer_p_eq> kmer_indexed_pairs_2pos;
 
 kmer_indexed_pairs unique_kmers;
 
@@ -177,27 +171,22 @@ bool contains_N(string& r) {
 	return r.find_first_of('N') != string::npos;
 }
 
-void index_read(string& r, pair_t* p, bool first) {
+void index_read1(string& r, pair_t* p) {
 	string temp = r.substr(0, k);
 	kmer_t* ukmer = new kmer_t(temp);
 	uchar pos = 0;
 	for (int i = k; i < r.length(); i++) {
 		auto it = unique_kmers.find(ukmer);
 		if (it != unique_kmers.end()) {
-			if (first) {
-				if (p->first1 == NULL) p->first1 = it->first;
-				p->last1 = it->first;
-			} else {
-				if (p->first2 == NULL) p->first2 = it->first;
-				p->last2 = it->first;
-			}
+			if (p->first1 == NULL) p->first1 = it->first;
+			p->last1 = it->first;
 			it->second.push_back(make_pair(p, pos));
 		}
 		if (i == r.length()) {
 			ukmer = NULL;
 			break;
 		}
-		ukmer->shift_left_and_insert(r[i++]);
+		ukmer->shift_left_and_insert(r[i]);
 		pos++;
 	}
 	delete ukmer;
@@ -218,9 +207,11 @@ void index_read2(pair_t* p, uchar pos1, kmer_indexed_pairs_2pos& r2index) {
 	for (int i = k; i < read->nucls; i++) {
 		auto it = unique_kmers.find(ukmer);
 		if (it != unique_kmers.end()) {
+			if (p->first2 == NULL) p->first2 = it->first;
+			p->last2 = it->first;
 			r2index[it->first].push_back(make_pair(p, make_pair(pos1, pos)));
 		}
-		ukmer->shift_left_and_insert(read_str[i++]);
+		ukmer->shift_left_and_insert(read_str[i]);
 		pos++;
 	}
 }
@@ -265,6 +256,8 @@ int main(int argc, char* argv[]) {
 	ifstream ukmersfile(argv[1]), fa1(argv[2]), fa2(argv[3]);
 
 	nucl_bm['A'] = 0, nucl_bm['C'] = 1, nucl_bm['G'] = 2, nucl_bm['T'] = 3;
+	rc_bm['T'] = 0, rc_bm['G'] = 1, rc_bm['C'] = 2, rc_bm['A'] = 3;
+
 
 	string ukmer;
 	while (getline(ukmersfile, ukmer)) {
@@ -274,38 +267,47 @@ int main(int argc, char* argv[]) {
 		}
 		kmer_t* kmer = new kmer_t(ukmer);
 		unique_kmers[kmer] = vector<pair<pair_t*, uchar> >();
+
+		kmer = new kmer_t(ukmer, true);
+		unique_kmers[kmer] = vector<pair<pair_t*, uchar> >();
 	}
 
-	string r1, r2;
+
+	unordered_map<pair_t*, string> pair_ids; // TODO: remove
+
+	string id1, r1, id2, r2;
 	vector<pair_t*> read_pairs;
-	while (getline(fa1, r1) && getline(fa2, r2)) {
+	while (getline(fa1, id1) && getline(fa2, id2)) {
 		getline(fa1, r1); getline(fa2, r2);
 
 		if (contains_N(r1) || contains_N(r2)) continue;
 
 		pair_t* p = new pair_t(new kmer_t(r1), new kmer_t(r2));
 		read_pairs.push_back(p);
-		index_read(r1, p, true);
-		//index_read(r2, p, false);
+		index_read1(r1, p);
+		pair_ids[p] = id1.substr(1, id1.length()-3);
 	}
 
 	unordered_map<pair_t*, vector<pair_t*> > edges;
 
-	for (auto it1 : unique_kmers) {
+	int seen = 0;
+	for (pair<kmer_t*, vector<pair<pair_t*, uchar> > > pairs_with_ukmer1 : unique_kmers) {
+		if (pairs_with_ukmer1.second.empty()) continue;
+
 		kmer_indexed_pairs_2pos index_by_r2;
-		vector<pair<pair_t*, uchar> >& pairs_with_ukmer1 = it1.second;
 		// index reads sharing a unique k-mer in reads 1 by unique k-mers in reads 2
-		for (auto it2 : pairs_with_ukmer1) {
-			pair_t* p = it2.first;
-			index_read2(p, it2.second, index_by_r2);
+		for (pair<pair_t*, uchar> pair_with_ukmer1 : pairs_with_ukmer1.second) {
+			pair_t* p = pair_with_ukmer1.first;
+			index_read2(p, pair_with_ukmer1.second, index_by_r2);
 		}
 
 		// process pairs sharing a unique k-mer in both reads
-		for (auto it2 : index_by_r2) {
-			vector<pair<pair_t*, pair<uchar, uchar> > >& pairs_with_ukmer2 = it2.second;
+		for (pair<kmer_t*, vector<pair<pair_t*, pair<uchar, uchar> > > > _pairs_with_ukmer2 : index_by_r2) {
+			vector<pair<pair_t*, pair<uchar, uchar> > >& pairs_with_ukmer2 = _pairs_with_ukmer2.second;
 			for (int i = 0; i < pairs_with_ukmer2.size(); i++) {
 				pair_t* pair1 = pairs_with_ukmer2[i].first;
-				if (pair1->first1 != it1.first && pair1->last1 != it1.first) continue;
+				if ((pair1->first1 != pairs_with_ukmer1.first && pair1->last1 != pairs_with_ukmer1.first) ||
+					(pair1->first2 != _pairs_with_ukmer2.first && pair1->last2 != _pairs_with_ukmer2.first)) continue;
 				pair<uchar, uchar> pair1_pos = pairs_with_ukmer2[i].second;
 				for (int j = i+1; j < pairs_with_ukmer2.size(); j++) {
 					pair_t* pair2 = pairs_with_ukmer2[j].first;
@@ -317,6 +319,9 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
+
+		if (seen % 10000 == 0) cerr << double(seen)/unique_kmers.size()*100 << "%" << endl;
+		seen++;
 	}
 
 	unordered_set<pair_t*> pairs_used;
@@ -325,10 +330,6 @@ int main(int argc, char* argv[]) {
 		if (pairs_used.count(p)) continue;
 		vector<pair_t*> comp;
 		get_component(p, edges, pairs_used, comp);
-		for (int i = 0; i < comp.size(); i++) {
-			for (int j = i+1; j < comp.size(); j++) {
-				cout << comp.size() << endl;
-			}
-		}
+		cout << comp.size() << endl;
 	}
 }
